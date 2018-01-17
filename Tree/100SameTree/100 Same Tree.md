2a1ed60a00b29461112e3aa208e9bacba4ff4fd7
lc <3160102243@zju.edu.cn>
2018-01-17 21:55:57 +0800

    easy

---
+++ Tree/100SameTree/100 Same Tree.md
@@ -0,0 +1,4 @@
+# 100 Same Tree
+
+* It is really a easy problem, and is easier than the problem in the PTA.
+* But the thought is the same - **recursion**
--- Tree/100SameTree/src/100.c
+++ Tree/100SameTree/src/100.c
@@ -6,6 +6,21 @@
  *     struct TreeNode *right;
  * };
  */
-bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
-    
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) 
+{
+	if(p == NULL && q == NULL)
+		return 1;
+
+	if(p == NULL && q != NULL || p != NULL && q == NULL)
+		return 0;
+
+	if(p->val == q->val)
+	{
+		if(isSameTree(p->left, q->left) && isSameTree(p->right, q->right))
+			return 1;
+		else
+			return 0;
+	}
+	else
+		return 0;
 }
