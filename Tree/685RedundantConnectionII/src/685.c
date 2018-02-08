1010b10da0720f776553635fa06b36fed23a84d3
lc <3160102243@zju.edu.cn>
2018-02-08 21:23:59 +0800

    unsolved

---
+++ Tree/685RedundantConnectionII/src/685.c
@@ -0,0 +1,8 @@
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findRedundantDirectedConnection(int** edges, int edgesRowSize, int edgesColSize, int* returnSize)
+{
+	
+}
